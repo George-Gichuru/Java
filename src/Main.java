96671bf78f5442407a00ce8f485148a6ed5bd7d8
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2023-01-31 20:09:28 +0300

    Java classes

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main (String[] args) {
         System.out.println("My First lines in Java");
         System.out.println("Oh My Days");
+        System.out.println("Salamamohammed");
     }
 }
